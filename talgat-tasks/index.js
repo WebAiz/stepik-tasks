a7ec68a28b56c452da5fdb623e348f1d0fc6f121
Aizada Turarova <aizada.turarova@gmail.com>
2022-04-27 10:47:08 +0600

    Make async await calls

--- talgat-tasks/index.js
+++ talgat-tasks/index.js
@@ -3,14 +3,36 @@ class Greeting {
         console.log("Hello")
         return this
     }
+
     salem() {
         console.log("Salem")
         return this
     }
+
     privet() {
         console.log("Privet");
         return this
     }
 }
+
 const greeting = new Greeting()
-greeting.salem().privet().salem()
+// greeting.salem().privet().salem()
+////////////////////////
+function wait(delay) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve()
+        }, delay)
+    })
+}
+
+async function exec() {
+    try {
+        await wait(1000);
+        console.log("Hello!"); // after 1 sec
+        await wait(2000);
+        console.log("Bye!"); // after 2 secs
+    } catch (err) {
+    }
+}
+exec()
