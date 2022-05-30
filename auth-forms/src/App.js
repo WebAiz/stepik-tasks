3953d087a4c5e8dd1b9f516ecf024e5d71c0c1be
Aizada.Turarova <aizada.turarova@gmail.com>
2022-05-30 14:39:00 +0600

    Add register page with react-hook-form

--- auth-forms/src/App.js
+++ auth-forms/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Login from './components/login';
+import Registration from "./components/registration/Regsitration";
 
 function App() {
   return (
     <div className="App">
       <Login/>
+      <Registration/>
     </div>
   );
 }
---
+++ auth-forms/src/components/registration/Regsitration.js
@@ -0,0 +1,105 @@
+import {LockClosedIcon} from '@heroicons/react/solid';
+import {useState} from 'react';
+import {validateRegex} from '../../util/helper';
+import {useForm} from 'react-hook-form';
+
+export default function Registration() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  return (
+    <>
+      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                 alt="Workflow" />
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Register your account</h2>
+            <p className="mt-2 text-center text-sm text-gray-600">
+              Or{' '}
+              <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                start your 14-day free trial
+              </a>
+            </p>
+          </div>
+          <form onSubmit={handleSubmit(onSubmit)} className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="firstname" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 rounded-t-md placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="FirstName"
+                  {...register('firstName', { required: true, maxLength: 20 })}
+                />
+                {errors.firstName && 'FirstName is required'}
+              </div>
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  type="number"
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Phone"
+                  {...register('phone', { min: 3, max: 12 })}
+                />
+                {errors.phone && 'Tel is required'}
+              </div>
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Email address"
+                  {...register('email', { required: true, maxLength: 30, pattern: /^[A-Za-z]+$/i })}
+                />
+                {errors.email && 'Emailis required'}
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                  {...register('password', { required: true, maxLength: 30 })}
+                />
+                {errors.password && 'password is required'}
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input onChange={(e) => setChecked(e.target.checked)} id="remember-me" name="remember-me"
+                       type="checkbox"
+                       className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
+                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                  Remember me
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <button type="submit"
+                      className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                  <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                </span>
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
