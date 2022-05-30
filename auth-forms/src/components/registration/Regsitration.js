4160897bc422e4bd3229c0ae0c697b6cb9842a9a
Aizada.Turarova <aizada.turarova@gmail.com>
2022-05-30 14:40:29 +0600

    Add register page with react-hook-form

--- auth-forms/src/components/registration/Regsitration.js
+++ auth-forms/src/components/registration/Regsitration.js
@@ -73,7 +73,7 @@ export default function Registration() {
 
             <div className="flex items-center justify-between">
               <div className="flex items-center">
-                <input onChange={(e) => setChecked(e.target.checked)} id="remember-me" name="remember-me"
+                <input id="remember-me" name="remember-me"
                        type="checkbox"
                        className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
                 <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
